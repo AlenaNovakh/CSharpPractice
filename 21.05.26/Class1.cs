31398446d3c30e4d71d5272526c6d5709e879395
AlenaNovakh <alenanovakh@yandex.ru>
2026-05-21 18:07:25 +0300

    Add files via upload

---
+++ 21.05.26/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21._05._26.Yadro
+{
+    public class Class1
+    {
+    }
+}
