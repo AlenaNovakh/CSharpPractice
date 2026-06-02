a1317481ccecf91f812d01dadbdfa330336aebee
AlenaNovakh <alenanovakh@yandex.ru>
2026-06-02 16:45:54 +0300

    Add files via upload

---
+++ 09.02.2026/ClassTask1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_семестр_финал
+{
+    public class ClassTask1
+    {
+        public struct Student
+            //сократить:
+           // public string Surname => _surname;
+        {
+            //поля
+            private string _surname;
+            private int _age;
+            private int[] _marks;
+            private int _debth;
+
+            //св-ва
+            public string Surname
+            {
+                get
+                {
+                    return _surname;
+                }
+            }
+            public int Age
+            {
+                get
+                {
+                    return _age;
+                }
+            }
+            public double AverageMark
+            {
+                get
+                {
+                    if (_marks == null || _marks.Length == 0)
+                    {
+                        return 0;
+                        
+                    }
+                    double average = 0;
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        average += _marks[i];
+                    }
+                    average /= _marks.Length;
+                    return average;
+                }
+            }
+            public bool IsExpelled => _debth >= 10;
+            public Student(string surname, int age)
+            {
+                //проицианилизировать ВСЕ поля
+                _surname = surname;
+                _age = age;
+                _marks = new int[0];
+                _debth = 0;
+            }
+
+        }
+        //КОНСТРУКТОР совпадает с название структуры
+        
+}
---
+++ 09.02.2026/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _2_семестр_финал
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassTask1.Student s1 = new ClassTask1.Student("Sidorov", 19);
+            Console.WriteLine(s1.Surname);
+            Console.WriteLine(s1.Age);
+
+            ClassTask1.Student s2 = new ClassTask1.Student("Petrov", 29);
+        }
+    }
+}
+// 1 УРОК
+//класс = шаблон
+//объект/экземпляр класса - конкретный пример (студент, ученик, работник)
+//!!не трогать шапку в лабораторной!! + все, что жирным - public
+//не забыть раскомментить тесты (и не забыть и в гитхабе это сделать)
+//проект - добавление нового элемента
+//internal менять на public
+//поля - как переменные, но всегда private
+//свойства - накидка на поля (get и set) (должны быть public, чтобы пользователь мог их получить)
+//конструктор 
+//
+//2 УРОК
+//проект - добавить уже существующий элемент (должен быть одинаковый namespace)
+//
+//
+//
